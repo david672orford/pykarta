41bce9db938f47d1a44feb4d74acfd77388c7822
David Chappell <David.Chappell@trincoll.edu>
2018-05-05 21:08:36 -0400

    included apartment number in the search

--- servers/modules/geocoder_openaddresses.py
+++ servers/modules/geocoder_openaddresses.py
@@ -1,6 +1,6 @@
 # pykarta/servers/modules/geocoder_openaddresses.py
 # Geocoder gets addresses from the MassGIS assessor's parcel map
-# Last modified: 4 May 2018
+# Last modified: 5 May 2018
 
 import os, urllib, json, time, re
 from pyspatialite import dbapi2 as db
@@ -19,36 +19,43 @@ def application(environ, start_response):
 		thread_data.cursor = cursor
 
 	query_string = urllib.unquote_plus(environ['QUERY_STRING'])
-	#stderr.write("QUERY_STRING: %s\n" % query_string)
 	house_number, apartment_number, street, town, state, postal_code = json.loads(query_string)
 
-	# Build the query and the list of address elements to be inserted into it
-	query = "SELECT longitude, latitude FROM addresses where house_number=? and street=? and town=? and state=?"
-	address = [
-		house_number,
+	# Build the query template
+	query_template = "SELECT longitude, latitude FROM addresses where {house} and street=? and town=? and state=?"
+	address_base = [
 		street,
 		town,
 		state
 		]
 	if postal_code is not None and postal_code != "":
-		query += " and (postal_code=? or postal_code is null)"
-		address.append(postal_code)
+		query_template += " and (postal_code=? or postal_code is null)"
+		address_base.append(postal_code)
 
-	# Try for an exact match
-	#start_time = time.time()
-	cursor.execute(query, address)
-	#stderr.write("elapsed: %f\n" % (time.time() - start_time))
-	row = cursor.fetchone()
+	# Result of SQL queries go here.
+	row = None
 
-	# If nothing found, look for a number range that matches.
-	if row is None and re.match(r'^\d+$', house_number):
-		query = query.replace(" house_number=? ", " house_number_start <= ? and house_number_end >= ? ")
-		house_number = int(house_number)
-		address = [house_number, house_number] + address[1:]
-		cursor.execute(query, address)
+	# If the apartment number is specified in the address, try a search with the exact house number and apartment number.
+	if apartment_number:
+		cursor.execute(query_template.replace("{house}" "apartment_number=? and house_number=?"), (apartment_number, house_number,) + address_base)
 		row = cursor.fetchone()
 
-	# If one or the other matched,
+	# If the previous search was not performed or did not produce anything, try without the apartment number.
+	if row is None:
+		# Try for an exact match 
+		#start_time = time.time()
+		cursor.execute(query_template.replace("{house}", "house_number=?"), (house_number,) + address_base)
+		#stderr.write("elapsed: %f\n" % (time.time() - start_time))
+		row = cursor.fetchone()
+
+	# If nothing found, look for an entry which gives a range of house numbers which includes this one.
+	if row is None and re.match(r'^\d+$', house_number):
+		house_number = int(house_number)
+		address = [house_number, house_number] + address[1:]
+		cursor.execute(query_template.replace("{house}", "house_number_start <= ? and house_number_end >= ?"), (house_number, house_number) + address_base)
+		row = cursor.fetchone()
+
+	# If we got a match, insert the latitude and longitude into a GeoJSON point object.
 	if row:
 		feature = {
 			'type':'Feature',
