f6b50990cc6d232e399a08f9ba6d5179cff53fc4
David Chappell <David.Chappell@trincoll.edu>
2019-10-17 16:23:17 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# https://packaging.python.org/tutorials/packaging-projects/
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyKarta",
+    version="0.92",
+    author="David Chappell",
+    author_email="David.Chappell@trincoll.edu",
+    description="Mapping library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/david672orford/pykarta",
+    packages=setuptools.find_packages(),
+	package_data={
+		"PyKarta":["draw/*.svg", "maps/layers/symbols/*.svg"],
+	},
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
